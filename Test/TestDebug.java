d25c325ec31e7c8488512ffc8a94c356c259b388
Bastien Faivre <faivre.bast@gmail.com>
2022-01-27 18:32:11 +0100

    added test debug

---
+++ Test/TestDebug.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestDebug {
+
+    @Test
+    void debug() {
+        assertEquals(0, 0);
+    }
+
+}

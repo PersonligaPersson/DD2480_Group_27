519788ae5e7cf5df20ccebf7bdb0789982ec2d94
Max Persson <max.e.persson@live.se>
2022-01-30 22:08:02 +0100

    Refactored the code to compute the euclidian distance in the plane by adding it to a separate function. I also wrote a helper function that determines if three points can be contained within a circle. This mainly refers to issue #4 and also issue #32.

--- LIConditions.java
+++ LIConditions.java
@@ -1,3 +1,5 @@
+package DD2480_Group_27;
+
 import java.util.*;
 
 public final class LIConditions {
@@ -48,6 +50,94 @@ public final class LIConditions {
         // return the results
         return conditions;
     }
+    
+    /**
+     * Computes the euclidian distance between two points in a plane.
+     * 
+     * @param x1 x-coordinate of the first point
+     * @param x2 x-coordinate of the second point
+     * @param y1 y-coordinate of the first point
+     * @param y2 y-coordinate of the second point
+     * @return
+     */
+    private double distance(double x1, double x2, double y1, double y2){
+        return Math.pow(x2-x1,2) + Math.pow(y2-y1,2);
+    }
+
+    /**
+     * This method returns the centerpoint between the points in a plane.
+     * 
+     * @param x1 x-coordinate of the first point
+     * @param x2 x-coordinate of the second point
+     * @param y1 y-coordinate of the first point
+     * @param y2 y-coordinate of the second point
+     * @return
+     */
+    private double[] getCenterpoint(double x1, double x2, double y1, double y2){
+        double c_x, c_y;        
+
+        // Compute the x-coordinate.
+        if(x1 > x2){
+            c_x = x1 - Math.abs(x1-x2)/2;
+        } else {
+            c_x = x1 + Math.abs(x1-x2)/2;
+        }
+
+        // Compute the y-coordinate.
+        if(y1 > y2){
+            c_y = y1 - Math.abs(y1-y2)/2;
+        } else {
+            c_y = y1 + Math.abs(y1-y2)/2;
+        }
+
+        // Return the coordinates.
+        return new double[]{c_x, c_y};
+    }
+
+    /**
+     * This method checks whether or not three points can be contained within a circle.
+     * 
+     * @param x1
+     * @param x2
+     * @param x3
+     * @param y1
+     * @param y2
+     * @param y3
+     * @return
+     */
+    private boolean canBeContained(double x1, double x2, double x3, double y1, double y2, double y3, double diameter){
+        // Compute the distance between the three points.            
+        double dist_p1p2 = distance(x1, x2, y1, y2);
+        double dist_p2p3 = distance(x2, x3, y2, y3);
+        double dist_p3p1 = distance(x3, x1, y3, y1);        
+
+        // Then check if any of the points are further away from each other than the circle's diameter. This would make it impossible to contain them in a circle.
+        if(dist_p1p2 > diameter || dist_p2p3 > diameter || dist_p3p1 > diameter)
+            return false;
+        
+        // If not, compute which points are furthest away from each other and compute the centerpoint.
+        double[] center_cords;
+        double[] remaining_point;
+        if(dist_p1p2 >= dist_p2p3 && dist_p1p2 >= dist_p3p1){
+            // In this case the centerpoint is drawn between points 1 and 2.
+            center_cords = getCenterpoint(x1, x2, y1, y2);
+            remaining_point = new double[]{x3,y3};
+        } else if(dist_p2p3 >= dist_p1p2 && dist_p2p3 >= dist_p3p1){
+            // In this case the centerpoint is drawn between points 2 and 3.
+            center_cords = getCenterpoint(x2, x3, y2, y3);
+            remaining_point = new double[]{x1,y1};
+        } else {
+            // In this case the centerpoint is drawn between points 3 and 1.
+            center_cords = getCenterpoint(x3, x1, y3, y1);
+            remaining_point = new double[]{x2,y2};
+        }
+
+        // At this point the remaining coordinate has to be at most the radius of the circle away from the centerpoint in order to be able to fit.
+        if(distance(center_cords[0], remaining_point[0], center_cords[1], remaining_point[1]) <= diameter/2)
+            return true;
+
+        return false;
+    }
 
     /**
      * Compare two doubles
@@ -81,10 +171,15 @@ public final class LIConditions {
         double lengthPowTwo = Math.pow(parameter.getLENGTH1(), 2);
         for (int i = 0; i < NUM_POINTS-1; i++) {
 
-            // Calculate Euclidean distance
-            distPowTwo = Math.pow(X_COORDINATES[i]-X_COORDINATES[i+1], 2)
-                            +Math.pow(Y_COORDINATES[i]-Y_COORDINATES[i+1], 2);
-            if (distPowTwo > lengthPowTwo) {
+            // // Calculate Euclidean distance
+            // distPowTwo = Math.pow(X_COORDINATES[i]-X_COORDINATES[i+1], 2) + Math.pow(Y_COORDINATES[i]-Y_COORDINATES[i+1], 2);
+            // if (distPowTwo > lengthPowTwo) {
+            //     return true;
+            // }
+
+            // Refactored to using the euclidian distance helper function.
+            distPowTwo = distance(X_COORDINATES[i], X_COORDINATES[i+1], Y_COORDINATES[i], Y_COORDINATES[i+1]);
+            if(distPowTwo > lengthPowTwo){
                 return true;
             }
 
@@ -100,7 +195,21 @@ public final class LIConditions {
     private boolean LIC_1() {
         boolean LIC_1 = false;
 
-        // TODO
+        // Start by checking for faulty input.
+        // Check for faulty parameters
+        if (NUM_POINTS != X_COORDINATES.length || NUM_POINTS != Y_COORDINATES.length || parameter.getRADIUS1() <= 0) {
+            return false;
+        }
+
+        // Declare the diameter of the circle.
+        double diameter = parameter.getRADIUS1()*2;
+
+        // Then iterate over all points and look for a tripple that can be contained within an enclosing circle.
+        for(int i=0; i < NUM_POINTS-2; i++){
+            LIC_1 = canBeContained(X_COORDINATES[i], X_COORDINATES[i+1], X_COORDINATES[i+2], Y_COORDINATES[i], Y_COORDINATES[i+1], Y_COORDINATES[i+2], diameter);
+            if(LIC_1)        
+                break;
+        }
 
         return LIC_1;
     }

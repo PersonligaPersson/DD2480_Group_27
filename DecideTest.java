457cdc6d00d1c54945d8440dd6ed755bbac0850c
Bastien Faivre <faivre.bast@gmail.com>
2022-01-31 02:33:48 +0100

    Updating code and doc of the test of the method computing the PUM, issue #40

--- DecideTest.java
+++ DecideTest.java
@@ -4,24 +4,67 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DecideTest {
 
+    /**
+     * The test checks the following:
+     *      - The resulting diagonal values are always true independently of the connector
+     *      - The ORR connector gives right results for every possible cases
+     *      - The ANDD connector gives right results for every possible cases
+     *      - The NOTUSED connector gives right results for every possible cases
+     */
     @Test
     void checkPUM() {
-        Decide.Connectors[][] LCM = new Decide.Connectors[][]{
-                {Decide.Connectors.ANDD, Decide.Connectors.ORR, Decide.Connectors.NOTUSED},
-                {Decide.Connectors.ORR, Decide.Connectors.ORR, Decide.Connectors.ANDD},
-                {Decide.Connectors.NOTUSED, Decide.Connectors.ANDD, Decide.Connectors.NOTUSED}
+        Decide.Connectors[][] LCM_ORR = new Decide.Connectors[][]{
+                {Decide.Connectors.ANDD, Decide.Connectors.ORR, Decide.Connectors.ORR, Decide.Connectors.ORR},
+                {Decide.Connectors.ORR, Decide.Connectors.ORR, Decide.Connectors.ORR, Decide.Connectors.ORR},
+                {Decide.Connectors.ORR, Decide.Connectors.ORR, Decide.Connectors.NOTUSED, Decide.Connectors.ORR},
+                {Decide.Connectors.ORR, Decide.Connectors.ORR, Decide.Connectors.ORR, Decide.Connectors.NOTUSED}
         };
-        boolean[] CMV = new boolean[]{true, false, true};
-        boolean[][] result = Decide.computePUM(LCM, CMV);
+        Decide.Connectors[][] LCM_ANDD = new Decide.Connectors[][]{
+                {Decide.Connectors.ANDD, Decide.Connectors.ANDD, Decide.Connectors.ANDD, Decide.Connectors.ANDD},
+                {Decide.Connectors.ANDD, Decide.Connectors.ORR, Decide.Connectors.ANDD, Decide.Connectors.ANDD},
+                {Decide.Connectors.ANDD, Decide.Connectors.ANDD, Decide.Connectors.NOTUSED, Decide.Connectors.ANDD},
+                {Decide.Connectors.ANDD, Decide.Connectors.ANDD, Decide.Connectors.ANDD, Decide.Connectors.NOTUSED}
+        };
+        Decide.Connectors[][] LCM_NOTUSED = new Decide.Connectors[][]{
+                {Decide.Connectors.ANDD, Decide.Connectors.NOTUSED, Decide.Connectors.NOTUSED, Decide.Connectors.NOTUSED},
+                {Decide.Connectors.NOTUSED, Decide.Connectors.ORR, Decide.Connectors.NOTUSED, Decide.Connectors.NOTUSED},
+                {Decide.Connectors.NOTUSED, Decide.Connectors.NOTUSED, Decide.Connectors.NOTUSED, Decide.Connectors.NOTUSED},
+                {Decide.Connectors.NOTUSED, Decide.Connectors.NOTUSED, Decide.Connectors.NOTUSED, Decide.Connectors.NOTUSED}
+        };
+        boolean[] CMV = new boolean[]{true, false, true, false};
+        boolean[][] result_ORR = Decide.computePUM(LCM_ORR, CMV);
+        boolean[][] result_ANDD = Decide.computePUM(LCM_ANDD, CMV);
+        boolean[][] result_NOTUSED = Decide.computePUM(LCM_NOTUSED, CMV);
 
-        boolean[][] expected = new boolean[][]{
-                {true, true, true},
-                {true, true, false},
-                {true, false, true}
+        boolean[][] expected_ORR = new boolean[][]{
+                {true, true, true, true},
+                {true, true, true, false},
+                {true, true, true, true},
+                {true, false, true, true}
         };
-        for (int i = 0; i < expected.length; ++i) {
-            assertArrayEquals(result[i], expected[i]);
+        boolean[][] expected_ANDD = new boolean[][]{
+                {true, false, true, false},
+                {false, true, false, false},
+                {true, false, true, false},
+                {false, false, false, true}
+        };
+        boolean[][] expected_NOTUSED = new boolean[][]{
+                {true, true, true, true},
+                {true, true, true, true},
+                {true, true, true, true},
+                {true, true, true, true}
+        };
+
+        for (int i = 0; i < expected_ORR.length; ++i) {
+            assertArrayEquals(result_ORR[i], expected_ORR[i]);
         }
+        for (int i = 0; i < expected_ANDD.length; ++i) {
+            assertArrayEquals(result_ANDD[i], expected_ANDD[i]);
+        }
+        for (int i = 0; i < expected_NOTUSED.length; ++i) {
+            assertArrayEquals(result_NOTUSED[i], expected_NOTUSED[i]);
+        }
+
     }
 
 } 

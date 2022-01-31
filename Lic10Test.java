c0da8bd30b5abe268dc167cbcb152f3772f9d49f
mellonnen <arvidgotthard98@gmail.com>
2022-01-31 17:06:27 +0100

    feat: extended and documented test cases

--- Lic10Test.java
+++ Lic10Test.java
@@ -10,7 +10,11 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 public class Lic10Test {
 
   @Test
-  void testLic10Positive() {
+  /**
+   * Positive test using a a 3-2-4 right angle triangle at the
+   * origin (exact area = 3).
+   */
+  void testLic10PositiveRightAngle() {
     Parameter parameter = new Parameter(0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0);
     double[] x = { 0, 1, 2, 1, 0 };
     double[] y = { 0, 1, 0, 2, 3 };
@@ -20,6 +24,25 @@ public class Lic10Test {
   }
 
   @Test
+  /**
+   * Positive test a tringale formed by the points (-3,2), (3,5), (6,4) (exact
+   * area = 37.5)
+   * 
+   */
+  void testLic10Positive() {
+    Parameter parameter = new Parameter(0, 0, 0, 37, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0);
+    double[] x = { -3, 1, 3, 2, 6 };
+    double[] y = { -2, 1, 5, 4, -4 };
+    int numPoints = 5;
+    LIConditions lic = new LIConditions(parameter, x, y, numPoints);
+    assertTrue(lic.computeAndGetConditions()[10]);
+  }
+
+  @Test
+  /**
+   * Negative test that where there are no trinagles large enough (Inputs supplied
+   * correctly).
+   */
   void testLic10Negative() {
     Parameter parameter = new Parameter(0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0);
     double[] x = { 0, 1, 2, 1, 0 };
@@ -30,7 +53,10 @@ public class Lic10Test {
   }
 
   @Test
-  void testLic10FaultyInput() {
+  /**
+   * Negative test to ensure enough points are supplied.
+   */
+  void testLic10ToFewPoints() {
     Parameter parameter = new Parameter(0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0);
     double[] x = { 0, 2, 0 };
     double[] y = { 0, 0, 2 };
@@ -38,5 +64,4 @@ public class Lic10Test {
     LIConditions lic = new LIConditions(parameter, x, y, numPoints);
     assertFalse(lic.computeAndGetConditions()[10]);
   }
-
 }

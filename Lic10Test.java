beb72dcdd4125a56a4f3722bc368c3635ddc2c9a
mellonnen <arvidgotthard98@gmail.com>
2022-02-01 08:26:31 +0100

    chore: fixed typo in LCI10 testcase

--- Lic10Test.java
+++ Lic10Test.java
@@ -71,7 +71,7 @@ public class Lic10Test {
    * 
    */
   void testLic10NegativeFaultyInputs() {
-    Parameter parameter = new Parameter(0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0);
+    Parameter parameter = new Parameter(0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 0, 0, 0, 0);
     double[] x = { 0, 1, 2, 1, 0, 3 };
     double[] y = { 0, 1, 0, 2, 2, 3 };
     int numPoints = 6;

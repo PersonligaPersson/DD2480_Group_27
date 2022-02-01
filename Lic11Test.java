1b00c1c9b1ceb8c72fbc24b6ed328ced7b70b813
Max Persson <max.e.persson@live.se>
2022-02-01 21:22:01 +0100

    Added functionality to compute and tests for LIC 11. This relates to issue #7.

--- Lic11Test.java
+++ Lic11Test.java
@@ -5,5 +5,55 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Lic11Test {
-    
+    /*
+    Verifies that LIC 11 is true for a valid sequence of points that meets the condition.
+    */
+    @Test
+    void lic11ValidSequenceValidCondition() {
+        Parameter parameter = new Parameter(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0);
+        double[] x = {2, 0, 0, 1};
+        double[] y = {0, 0, 0, 0};
+        int numPoints = 4;
+        LIConditions lic = new LIConditions(parameter, x, y, numPoints);
+        assertTrue(lic.computeAndGetConditions()[11]);
+    }
+
+    /*
+    Verifies that LIC 11 is false for a valid sequence of points that does not meet the condition.
+    */
+    @Test
+    void lic11ValidSequenceInvalidCondition() {
+        Parameter parameter = new Parameter(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0);
+        double[] x = {1, 0, 0, 2};
+        double[] y = {0, 0, 0, 0};
+        int numPoints = 4;
+        LIConditions lic = new LIConditions(parameter, x, y, numPoints);
+        assertFalse(lic.computeAndGetConditions()[11]);
+    }
+
+    /*
+    Verifies that LIC 11 is false for an invalid sequence of points.
+    */ 
+    @Test
+    void lic11InvalidSequence() {
+        Parameter parameter = new Parameter(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0);
+        double[] x = {2, 0, 0, 1};
+        double[] y = {0, 0, 0, 0};
+        int numPoints = 4;
+        LIConditions lic = new LIConditions(parameter, x, y, numPoints);
+        assertFalse(lic.computeAndGetConditions()[11]);
+    }
+
+    /*
+    Verifies that LIC 11 is false for an invalid sequence with too few points.
+    */ 
+    @Test
+    void lic11TooFewPoints() {
+        Parameter parameter = new Parameter(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0);
+        double[] x = {2, 0};
+        double[] y = {0, 0};
+        int numPoints = 2;
+        LIConditions lic = new LIConditions(parameter, x, y, numPoints);
+        assertFalse(lic.computeAndGetConditions()[11]);
+    }
 }
